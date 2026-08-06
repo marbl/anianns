d15a5ec9b59b62b2f5a91352e4188e2618c6dd7d
Alex Sweeten <alex.sweeten@nih.gov>
2026-08-05 20:05:11 -0600

    Bump version to 0.7.1

--- src/anianns/const.py
+++ src/anianns/const.py
@@ -26,4 +26,4 @@ BED_COLUMNS = [
 DESCRIPTION = "Ani Ann's: ANI Inferred ANNotation of Tandem Repeats"
 HEADER_FORMAT = "32sQ"  # 32-byte name (padded), 8-byte unsigned int (number of kmers)
 HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
-VERSION = "0.7.0"
+VERSION = "0.7.1"
